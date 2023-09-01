830799178a9d8522caf19780392cf4ab0e5da75f
PraveenDev111 <97358239+PraveenDev111@users.noreply.github.com>
2023-09-01 12:40:11 +0530

    Update constants.dart

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 const String google_api_key = 'AIzaSyDt8YDXSOJOtbzWb3XT97SNksJwvjq1WPQ';
 const Color primaryColor = Colors.deepPurple;
 const double defaultpadding = 16.0;
+double mobileDeviceWidth = 0.0;
+double mobileDeviceHeight = 0.0;
+const Color buttonColor = Color.fromARGB(255, 43, 166, 129);
+const Color bgcolor = Color.fromARGB(255, 110, 237, 199);
