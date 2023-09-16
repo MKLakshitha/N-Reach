15ea360a4eadadd3eb321715084dbf735cd83b87
PraveenDev111 <97358239+PraveenDev111@users.noreply.github.com>
2023-09-16 22:43:54 +0530

    Update about.dart

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:simple_flutter_app/constants/constants.dart';
+import 'constants.dart';
+import 'btmnavbar.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class About extends StatelessWidget {
   const About({super.key});
@@ -9,11 +11,9 @@ class About extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white.withOpacity(0),
-        title: const Text(
-          'hi',
-          style: blackText,
-        ),
       ),
+      bottomNavigationBar:
+          BtmNavBar(currentIndex: 2, onItemSelected: (int n) {}),
       extendBodyBehindAppBar: true,
       body: SingleChildScrollView(
         child: Column(
@@ -25,22 +25,27 @@ class About extends StatelessWidget {
               fit: BoxFit.cover,
             ),
             Padding(
-              padding: EdgeInsets.all(mobileDeviceWidth * 0.04),
+              padding: EdgeInsets.only(
+                  top: mobileDeviceWidth * 0.04,
+                  left: mobileDeviceWidth * 0.04,
+                  right: mobileDeviceWidth * 0.04),
               child: Column(
                 children: [
-                  const Text(
+                  Text(
                     'NSBM Green University',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    style: TextStyle(
+                        fontSize: mobileDeviceWidth < 370 ? 23 : 26,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const Text(
+                    '100% Government owned global level university',
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
                     textAlign: TextAlign.center,
                   ),
                   Divider(
                     indent: mobileDeviceWidth * 0.05,
                     endIndent: mobileDeviceWidth * 0.05,
-                  ),
-                  const Text(
-                    '100% Government owned global level university',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                    textAlign: TextAlign.center,
                   ),
                 ],
               ),
@@ -50,10 +55,105 @@ class About extends StatelessWidget {
               child: Column(
                 children: [
                   Container(
-                    padding: EdgeInsets.all(mobileDeviceWidth * 0.03),
+                    padding: EdgeInsets.all(mobileDeviceWidth * 0.05),
                     width: mobileDeviceWidth,
-                    color: const Color.fromARGB(255, 209, 209, 209),
-                    child: const Column(),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 240, 240, 240),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Our Vision',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'To be Sri Lanka’s best-performing graduate school and to be recognized internationally.',
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  ),
+                  const Divider(),
+                  Container(
+                    padding: EdgeInsets.all(mobileDeviceWidth * 0.05),
+                    width: mobileDeviceWidth,
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 240, 240, 240),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Our Mision',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'To develop globally competitive and responsible graduates that businesses demand, working in synergy with all our stakeholders and contributing to our society.',
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  ),
+                  const Divider(),
+                  const Text(
+                    'Get connected with NSBM on socials\n',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () async {
+                          // ignore: deprecated_member_use
+                          if (await canLaunch(
+                              'https://www.instagram.com/nsbmgreenuniversity')) {
+                            // ignore: deprecated_member_use
+                            await launch(
+                                'https://www.instagram.com/nsbmgreenuniversity');
+                          } else {
+                            throw 'Could not launch https://www.instagram.com/nsbmgreenuniversity';
+                          }
+                        },
+                        child: Image.asset('assets/inst.png',
+                            height: mobileDeviceHeight * 0.07),
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          // ignore: deprecated_member_use
+                          if (await canLaunch(
+                              'https://web.facebook.com/nsbm.lk')) {
+                            // ignore: deprecated_member_use
+                            await launch('https://web.facebook.com/nsbm.lk');
+                          } else {
+                            throw 'Could not launch https://web.facebook.com/nsbm.lk';
+                          }
+                        },
+                        child: Image.asset('assets/fb.png',
+                            height: mobileDeviceHeight * 0.07),
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          // ignore: deprecated_member_use
+                          if (await canLaunch('https://www.nsbm.ac.lk')) {
+                            // ignore: deprecated_member_use
+                            await launch('https://www.nsbm.ac.lk');
+                          } else {
+                            throw 'Could not launch https://www.nsbm.ac.lk';
+                          }
+                        },
+                        child: Image.asset('assets/web.png',
+                            height: mobileDeviceHeight * 0.07),
+                      )
+                    ],
                   )
                 ],
               ),
